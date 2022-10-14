de2ccecb629368eb86d318af3c5a4d2e9f6e9587
SkavenLord58 <adamkrasa@sd58.cz>
2022-10-14 16:39:43 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from discord import Message
+from discord.ext.commands import Context
+from dotenv import load_dotenv
+from discord.ext import commands
+
+import requests
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+TEXT_SYNTH_TOKEN = os.getenv('TEXT_SYNTH_TOKEN')
+PREFIX = os.getenv('BOT_PREFIX')
+
+client = commands.Bot(command_prefix=PREFIX)
+
+@client.command()
+async def ping(ctx: Context):
+    m = await ctx.send(f'Ping?')
+    ping = int(str(m.created_at - ctx.message.created_at).split(".")[1]) / 1000
+    await m.edit(content=f'Pong! Latency is {ping}ms. API Latency is {round(client.latency * 1000)}ms.')
+    pass
+
+client.run(TOKEN)
