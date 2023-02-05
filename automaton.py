c3acfdd2949fb525c6364af8fe39eda3f4d13aa3
SkavenLord58 <adamkrasa@sd58.cz>
2023-02-05 14:07:40 +0100

    feat: cron-like feature

--- automaton.py
+++ automaton.py
@@ -1,12 +1,12 @@
 ## DecimAutomation main functionality
-# parsing the cronjob file
+# parsing the "cronjob" file
 import json
 from datetime import datetime as dt
 
 TARGETS = {
         "decimautomation" : "DecimAutomation#4633",
-        "decimbot2": "DecimBOT 2.0#8467",
-        "basedschizo" : "Based Schizo#7762"
+        "decimbot2": 996439005405126787,
+        "basedschizo" : 1044650088494010478
     }
 
 INTERVALS = {
@@ -29,21 +29,42 @@ class Automaton:
                 if line[0] == "#":
                     pass
                 elif line[0] == "@":
-                    self.jobs.append(line.split(" "))
+                    self.jobs.append(line.replace("\n","").split(" "))
         print(f"Job list parsed. Number of jobs: {len(self.jobs)}\nList of jobs:\n{self.print_jobs()}")
 
+    def work_job(self, job) -> str:
+        print(f"Processing job: {job[1]}|{job[2]}...")
+        f = open("./jobslock.json", encoding="utf-8", mode="r")
+        lf = json.load(f)
+        f.close()
+        lf.update(
+                {
+                    job[1]:
+                     {
+                        job[2] : dt.now().timestamp().__int__()
+                        }
+                })
+        f = open("./jobslock.json", encoding="utf-8", mode="w")
+        json.dump(lf, f)
+        f.close()
+        
+        return f"{TARGETS[job[1]]};{job[2]}"
 
     # working on jobs loop
     def precheck_jobs(self):
+        results = []
         for job in self.jobs:
             result = self.check_job_done(name=job[2], target=job[1], interval=job[0])
             if result:
+                results.append(self.work_job(job))
+            else:
                 pass
+        return results
 
     # check if job was already done
     def check_job_done(self, name: str = None, target: str = None, interval: str = None) -> bool:
-        with open("./jobs.lock", encoding="utf-8", mode="r+") as f:
-            lf : dict = json.load(f)
+        with open("./jobslock.json", encoding="utf-8", mode="r") as f:
+            lf = json.load(f)
             try:
               if lf[target][name] and \
                       dt.now().timestamp().__int__() - lf[target][name] > INTERVALS[interval]:
@@ -59,9 +80,8 @@ class Automaton:
     def print_jobs(self):
         joblist = ""
         for job in self.jobs:
-            joblist += f"Job {self.jobs.index(job)}: {job[0]} {job[1]} {job[2]}"
+            joblist += f"Job {self.jobs.index(job)}: {job[0]} {job[1]} {job[2]}\n"
         return joblist
 
 if __name__ == "__main__":
-    
     pass
--- main.py
+++ main.py
@@ -5,20 +5,54 @@ from disnake.ext.commands import Context
 from dotenv import load_dotenv
 from disnake.ext import tasks, commands
 import requests
+from automaton import Automaton
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('BOT_PREFIX')
+PREFIXES = {
+    "996439005405126787" : "$",
+    "1044650088494010478" : "§"
+}
+CHANNELS = {
+    "itpero": 786625189038915625
+}
+
+autoserv = Automaton()
 
 # add intents for bot and command prefix for classic command support
 intents = disnake.Intents.all()
 client = disnake.ext.commands.Bot(command_prefix=PREFIX, intents=intents)
 
+async def process_command(prefix, command: list):
+    cc = command.split("-")
+    try:
+        if cc[1]:
+            pass
+    except Exception as exc:
+        cc[1] = 932301697836003358
+    
+    channel = client.get_channel(CHANNELS[cc[1]])
+    m = await channel.send(f"{prefix}{cc[0]}")
+    await m.delete()
+
+async def work_loop(jobs):
+    if not jobs:
+        pass
+    else:
+        for job in jobs:
+            target, command = job.split(";")
+            if target == "DecimAutomation#4633":
+                eval(f"{command}()")
+            else:
+                await process_command(PREFIXES[target], command)
+        pass
+
 # running job check
 @tasks.loop(seconds = 30)
 async def check_jobs_loop():
     print("Running job checker!")
-    pass
+    await work_loop(autoserv.precheck_jobs())
     
 @check_jobs_loop.before_loop
 async def before_check_jobs():
@@ -28,11 +62,11 @@ async def before_check_jobs():
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    # print(check_cronjobs())
+    autoserv.parse_cronjobs()
     if check_jobs_loop.start():
-        print("DecimAutomation running...")
+        print("Automaton running...")
     else:
-        print("DecimAutomation failed to run.")
+        print("Automaton failed to run.")
     
 
 ## other DecimAutomation platform commands
